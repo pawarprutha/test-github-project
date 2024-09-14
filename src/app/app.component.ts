96c8baf78718b92f812e7d4bd933417970f5b938
prutha.pawar-at-472600869960 <prutha.pawar@wsfx.in>
2024-09-14 11:27:20 +0530

    changes for food report app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
 import { Component } from '@angular/core';
+import { BaseProvider } from './services/food-report.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [BaseProvider]
 })
 export class AppComponent {
-  title = 'github-project new';
+  reports: any;
+  public foodReport: any[] = [];
+  fineForBreakfast: number | undefined;
+  fineForLunch: number | undefined;
+  fineForDinner: number | undefined;
+  totalFineForDay: number | undefined;
+  response: any;
+  totalMonthlyFine: number = 0;
+  title = 'Food Consumption Report App';
+  
+  constructor(
+    public baseProvider : BaseProvider) {
+      this.getFoodReports();
+    }
+
+  getFoodReports() {
+    this.baseProvider.makePostCall().subscribe(resp=>{
+      this.response = resp.body;
+      this.reports = this.response.reports;
+      this.reports.forEach((singlePurpose: any) => {
+        if(singlePurpose.opt_ins.breakfast == 'Pending') {
+          this.fineForBreakfast = 100;
+        } else {
+          this.fineForBreakfast = 0;
+        }
+        if(singlePurpose.opt_ins.lunch == 'Pending') {
+          this.fineForLunch = 100;
+        } else {
+          this.fineForLunch = 0;
+        }
+        if(singlePurpose.opt_ins.dinner == 'Pending') {
+          this.fineForDinner = 100;
+        } else {
+          this.fineForDinner = 0;
+        }
+        this.totalFineForDay =  this.fineForBreakfast + this.fineForLunch + this.fineForDinner;
+        singlePurpose.totalFineForDay = this.totalFineForDay;
+        if(this.totalFineForDay != 0) {
+          this.totalMonthlyFine = this.totalFineForDay + this.totalMonthlyFine;
+        }
+        this.foodReport?.push(singlePurpose);
+        });
+    }, error=>{
+      console.log(error);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BaseProvider } from './services/food-report.service';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [HttpClient, BaseProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
